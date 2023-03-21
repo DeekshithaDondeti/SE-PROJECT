1bc0e5f5669fcadd5ec764a9c61281767e416c03
DeekshithaDondeti <deekshithadondeti19@gmail.com>
2023-03-21 19:54:34 +0530

    a short description of the change

--- code.py
+++ code.py
@@ -1,8 +1,33 @@
-print("hello")
+import pandas as pd
 
-print("gayathri edited this")
-print("hi")
+df = pd.read_csv('dataset.csv')
 
-print("online Travel Booking")
-print('edited')
+print(df.columns.values)
+print(df.shape[0])
+
+state_city = {}
+city_place = {}
+
+for i in range(0,df.shape[0]):
+    if df.iloc[i]['State'] not in state_city.keys():
+        state_city[df.iloc[i]['State']] = [df.iloc[i]['City']]
+    elif df.iloc[i]['State'] in state_city.keys() and df.iloc[i]['City'] not in state_city[df.iloc[i]['State']]:
+        state_city[df.iloc[i]['State']].append(df.iloc[i]['City'])
+
+for i in range(0,df.shape[0]):
+    if df.iloc[i]['City'] not in city_place.keys():
+        city_place[df.iloc[i]['City']] = [df.iloc[i]['Name']]
+    elif df.iloc[i]['City'] in city_place.keys() and df.iloc[i]['Name'] not in city_place[df.iloc[i]['City']]:
+        city_place[df.iloc[i]['City']].append(df.iloc[i]['Name'])
+
+# for i in state_city:
+#     print(i,state_city[i])
+#
+# print( '-------------------------------------------')
+#
+# for i in city_place:
+#     print(i,city_place[i])
+
+
+
 
