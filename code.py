70caf5a8e7ac53066bba47b6202a1fe371c3dd62
Gayathri Chilukuri <chgayatri77@gmail.com>
2023-03-31 01:35:09 +0530

    changes made

--- code.py
+++ code.py
@@ -2,9 +2,6 @@ import pandas as pd
 import requests
 df = pd.read_csv('dataset.csv')
 
-print(df.columns.values)
-print(df.shape[0])
-
 state_city = {}
 city_place = {}
 
@@ -34,29 +31,71 @@ for i in range(0,df.shape[0]):
 # for i in city_place:
 #     print(i,city_place[i])
 
-locpoints= ["77.983936,28.255904","77.05993,28.487555","77.15993,28.587555","77.25993,28.687555"]
-p1 = "https://apis.mapmyindia.com/advancedmaps/v1/2dfdb55d-3f12-4739-bdc5-2d85c76fcd57/distance_matrix/driving/"
+startpoint = "77.7777,28.6888"
+locpoints= ["77.983936,28.255904","77.15993,28.587555","77.05993,28.487555","77.25993,28.687555"]
+p1 = "https://apis.mapmyindia.com/advancedmaps/v1/75509e0f-f849-4a0d-9f07-b57196878308/distance_matrix/driving/"
+p11 = ""
 p2 = "?sources="
 p3 = "&destinations="
 for i in range(len(locpoints)):
     if i == len(locpoints)-1:
-        p1 = p1+locpoints[i]
+        p11 = p11+locpoints[i]
         p2 = p2 + str(i)
         p3 = p3 + str(i)
     else:
-        p1 = p1+locpoints[i]+";"
+        p11 = p11+locpoints[i]+";"
         p2 = p2 + str(i) + ";"
         p3 = p3 + str(i) + ";"
 
-response = requests.get(p1+p2+p3, headers={'Authorization': '2dfdb55d-3f12-4739-bdc5-2d85c76fcd57'})
+dis = []
+startres = requests.get("https://apis.mapmyindia.com/advancedmaps/v1/75509e0f-f849-4a0d-9f07-b57196878308/distance_matrix/driving/"+startpoint+p11+"?rtype=0&region=ind")
+
+
+
+response = requests.get(p1+p11+p2+p3, headers={'Authorization': '75509e0f-f849-4a0d-9f07-b57196878308'})
 d = response.json()
+print(d)
 dismat = []
 durmat = []
 for i in d['results']['distances']:
     dismat.append(i)
 for i in d['results']['durations']:
     durmat.append(i)
+
 print(dismat)
 print(durmat)
+
+def tsp(path, graph, visited, cost, min_cost, min_path):
+
+    if len(path) == len(graph):
+        if cost < min_cost:
+            min_cost = cost
+            min_path = path
+        return min_path, min_cost
+
+    for node in range(len(graph)):
+        if not visited[node]:
+            visited[node] = True
+            new_path = path + [node]
+            new_cost = cost + graph[path[-1]][node]
+            min_path, min_cost = tsp(new_path, graph, visited, new_cost, min_cost, min_path)
+            visited[node] = False
+
+    return min_path, min_cost
+
+def traveling_salesman(graph, source):
+
+    min_cost = float("inf")
+    min_path = None
+    visited = [False] * len(graph)
+
+    visited[source] = True
+    path, cost = tsp([source], graph, visited, 0, min_cost, min_path)
+    visited[source] = False
+
+    return path
+
+graph = durmat
+print(traveling_salesman(graph,2))
 
 
