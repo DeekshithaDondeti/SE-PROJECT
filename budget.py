46292d635c3fc59b2c51d12935710a1a89deb30f
Gayathri Chilukuri <chgayatri77@gmail.com>
2023-05-17 23:33:05 +0530

    changes made 1

---
+++ budget.py
@@ -0,0 +1,11 @@
+totalbudget = 50000
+days = 5
+ast = 5000
+asf = 1000
+assh = 5000
+amis = 2000
+atra = 500
+if ast+asf+assh+amis+atra >= totalbudget/days:
+    print("You are going off the limit")
+else:
+    print("great going, you are following your budget")
